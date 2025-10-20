7369118faf145374b96d83108d4eb452900a2ffe
Markus-Kivinen <markus.kivinen2@gmail.com>
2025-10-20 20:50:51 +0300

    remove utime and gc.collect

--- pico_i2c_lcd.py
+++ pico_i2c_lcd.py
@@ -1,8 +1,8 @@
-import utime
-import gc
+import time
+
+from machine import I2C
 
 from lcd_api import LcdApi
-from machine import I2C
 
 # PCF8574 pin definitions
 MASK_RS = 0x01       # P0
@@ -20,23 +20,22 @@ class I2cLcd(LcdApi):
         self.i2c = i2c
         self.i2c_addr = i2c_addr
         self.i2c.writeto(self.i2c_addr, bytes([0]))
-        utime.sleep_ms(20)   # Allow LCD time to powerup
+        time.sleep_ms(20)   # Allow LCD time to powerup
         # Send reset 3 times
         self.hal_write_init_nibble(self.LCD_FUNCTION_RESET)
-        utime.sleep_ms(5)    # Need to delay at least 4.1 msec
+        time.sleep_ms(5)    # Need to delay at least 4.1 msec
         self.hal_write_init_nibble(self.LCD_FUNCTION_RESET)
-        utime.sleep_ms(1)
+        time.sleep_ms(1)
         self.hal_write_init_nibble(self.LCD_FUNCTION_RESET)
-        utime.sleep_ms(1)
+        time.sleep_ms(1)
         # Put LCD into 4-bit mode
         self.hal_write_init_nibble(self.LCD_FUNCTION)
-        utime.sleep_ms(1)
+        time.sleep_ms(1)
         LcdApi.__init__(self, num_lines, num_columns)
         cmd = self.LCD_FUNCTION
         if num_lines > 1:
             cmd |= self.LCD_FUNCTION_2LINES
         self.hal_write_command(cmd)
-        gc.collect()
 
     def hal_write_init_nibble(self, nibble):
         # Writes an initialization nibble to the LCD.
@@ -44,17 +43,14 @@ class I2cLcd(LcdApi):
         byte = ((nibble >> 4) & 0x0f) << SHIFT_DATA
         self.i2c.writeto(self.i2c_addr, bytes([byte | MASK_E]))
         self.i2c.writeto(self.i2c_addr, bytes([byte]))
-        gc.collect()
         
     def hal_backlight_on(self):
         # Allows the hal layer to turn the backlight on
         self.i2c.writeto(self.i2c_addr, bytes([1 << SHIFT_BACKLIGHT]))
-        gc.collect()
         
     def hal_backlight_off(self):
         #Allows the hal layer to turn the backlight off
         self.i2c.writeto(self.i2c_addr, bytes([0]))
-        gc.collect()
         
     def hal_write_command(self, cmd):
         # Write a command to the LCD. Data is latched on the falling edge of E.
@@ -68,8 +64,7 @@ class I2cLcd(LcdApi):
         self.i2c.writeto(self.i2c_addr, bytes([byte]))
         if cmd <= 3:
             # The home and clear commands require a worst case delay of 4.1 msec
-            utime.sleep_ms(5)
-        gc.collect()
+            time.sleep_ms(5)
 
     def hal_write_data(self, data):
         # Write data to the LCD. Data is latched on the falling edge of E.
@@ -83,4 +78,3 @@ class I2cLcd(LcdApi):
                 ((data & 0x0f) << SHIFT_DATA))      
         self.i2c.writeto(self.i2c_addr, bytes([byte | MASK_E]))
         self.i2c.writeto(self.i2c_addr, bytes([byte]))
-        gc.collect()
